1f5e98a5944b4acf038afc3eff03b366a604cb6a
Abdullahalbakhit <abdullahalbakhit5@gmail.com>
2022-09-07 07:10:40 +0300

    almost dine #2

--- js/script.js
+++ js/script.js
@@ -1,8 +1,91 @@
-
-console.log("running")
-
 let navbar = document.querySelector('.header .navbar');
 
+let carts = document.querySelectorAll('.add-cart');
+
+let product = [
+    {
+        name: 'Gray Tshirt',
+        tag: 'greytgshirt',
+        price: 15,
+        incart: 0,
+    },
+    {
+        name: 'Gray Hoddie',
+        tag: 'greytghoddie',
+        price: 20,
+        incart: 0,
+    },
+    {
+        name: 'Black Tshirt',
+        tag: 'blacktshirt',
+        price: 15,
+        incart: 0,
+    },
+    {
+        name: 'black Hoddie',
+        tag: 'blackhoddie',
+        price: 20,
+        incart: 0,
+    }
+];
+
+for (let i=0; i < carts.length; i++) {
+    carts[i].addEventListener('click', () => {
+        cartNumbers(product[i]);
+    })
+}
+
+function onLoadCartNumbers() {
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    if(productNumbers) {
+        document.querySelector('.cart span').textContent = productNumbers;
+    }
+}
+
+function cartNumbers(product) {
+
+    let productNumbers = localStorage.getItem('cartNumbers');
+    
+    productNumbers = parseInt(productNumbers);
+
+    if( productNumbers ) {
+        localStorage.setItem('cartNumbers', productNumbers + 1);
+        document.querySelector('.cart span').textContent = productNumbers + 1;
+    } else {
+        localStorage.setItem('cartNumbers', 1);
+        document.querySelector('.cart span').textContent = 
+        1;
+    }
+
+    setItems(product);
+}
+
+function setItems(product) {
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+
+    if(cartItems != null) {
+
+        if(cartItems[product.tag] == undefined) {
+            cartItems = {
+                ...cartItems,
+                [product.tag]: product
+            }
+        }
+        cartItems[product.tag].inCart += 1;
+    } else {
+        product.inCart = 1;
+        cartItems = {
+            [product.tag]: product
+        }
+    }
+    
+
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+}
+
+onLoadCartNumbers(); 
 
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
